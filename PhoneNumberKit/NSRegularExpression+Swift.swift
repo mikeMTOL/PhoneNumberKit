64de291273a1f2ba0f996616f35d207be2e379c8
mike <mmusallam@gmail.com>
2017-08-02 04:12:50 -0700

    Swift 4 fixes

--- PhoneNumberKit/NSRegularExpression+Swift.swift
+++ PhoneNumberKit/NSRegularExpression+Swift.swift
@@ -11,8 +11,8 @@ import Foundation
 extension String {
 	func nsRange(from range: Range<String.Index>) -> NSRange {
 		let utf16view = self.utf16
-		let from = range.lowerBound.samePosition(in: utf16view)
-		let to = range.upperBound.samePosition(in: utf16view)
+		guard let from = range.lowerBound.samePosition(in: utf16view),
+            let to = range.upperBound.samePosition(in: utf16view) else { return NSMakeRange(0, 0) }
 		return NSMakeRange(utf16view.distance(from: utf16view.startIndex, to: from),
 		                   utf16view.distance(from: from, to: to))
 	}
